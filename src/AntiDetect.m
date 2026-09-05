6f29953730f2693c3076d757af61516e82adfe8e
isisks29 <3637065792@qq.com>
2026-09-05 21:02:06 +0800

    Update AntiDetect.m

--- src/AntiDetect.m
+++ src/AntiDetect.m
@@ -114,15 +114,6 @@ static int hook_stat(const char *path, void *buf) {
 
 @implementation AntiDetect
 
-+ (void)installAll {
-    [self installPtraceHook];
-    [self installSysctlHook];
-    [self installDyldHide];
-    [self installJailbreakHide];
-    [AntiDetect runInjectorScan];
-    [AntiDetect denyDebug];
-}
-
 static AntiDetect *_g_instance = nil;
 + (instancetype)sharedInstance {
     static dispatch_once_t onceToken;
