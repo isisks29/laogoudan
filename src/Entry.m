0a6f17e953e49b79b764b149bb977e0357263850
isisks29 <3637065792@qq.com>
2026-09-05 18:05:25 +0800

    Create Entry.m

---
+++ src/Entry.m
@@ -0,0 +1,23 @@
+// src/Entry.m
+#include <dlfcn.h>
+#import <Foundation/Foundation.h>
+#import "Config.h"
+#import "AntiDetect.h"
+#import "FeatureManager.h"
+#import "MacroManager.h"
+#import "TweakUI.h"
+
+__attribute__((constructor))
+void dylib_initialize(void)
+{
+    @autoreleasepool {
+        [[AntiDetect sharedInstance] startProtect];
+        [[Config shared] loadConfig];
+        [[FeatureManager sharedManager] setup];
+        [[MacroManager shared] setup];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [TweakUI showFloatingWindow];
+        });
+    }
+}
