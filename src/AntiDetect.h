88f4000c11e2e26315954aef8816fced19a6987b
isisks29 <3637065792@qq.com>
2026-09-06 00:05:52 +0800

    Update AntiDetect.h

--- src/AntiDetect.h
+++ src/AntiDetect.h
@@ -1,22 +1,9 @@
 #import <Foundation/Foundation.h>
-
 #ifndef ANTI_DETECT_H
 #define ANTI_DETECT_H
-
 @interface AntiDetect : NSObject
 + (instancetype)sharedInstance;
 - (void)startProtect;
-
 + (void)installAll;
-+ (void)installPtraceHook;
-+ (void)installSysctlHook;
-+ (void)installDyldHide;
-+ (void)installJailbreakHide;
 @end
-
-@interface AntiDetect (Injector)
-+ (void)runInjectorScan;
-+ (void)denyDebug;
-@end
-
-#endif //❗❗这个 #endif 千万不能丢，头文件保护闭合
+#endif
