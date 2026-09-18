3c7f92ad5074b05da72d5a108636b51449441b3b
isisks29 <3637065792@qq.com>
2026-09-19 01:15:20 +0800

    Update Entry.m

--- src/Entry.m
+++ src/Entry.m
@@ -1,44 +1,9 @@
 #include <dlfcn.h>
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "Config.h"
-#import "MacroManager.h"
-#import "UI/TweakUI.h"
-
-static void doInitialize(void);
 
 __attribute__((constructor))
 void dylib_initialize(void)
 {
-    @autoreleasepool {
-        [[GlobalConfig shared] load];
-        __block id observer = nil;
-        observer = [[NSNotificationCenter defaultCenter]
-            addObserverForName:UIApplicationDidFinishLaunchingNotification
-                        object:nil
-                         queue:[NSOperationQueue mainQueue]
-                    usingBlock:^(NSNotification *note) {
-                        if (observer) {
-                            [[NSNotificationCenter defaultCenter] removeObserver:observer];
-                            observer = nil;
-                        }
-                        doInitialize();
-                    }];
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC),
-                       dispatch_get_main_queue(), ^{
-            if (observer) {
-                [[NSNotificationCenter defaultCenter] removeObserver:observer];
-                observer = nil;
-                doInitialize();
-            }
-        });
-    }
-}
-
-static void doInitialize(void) {
-    @autoreleasepool {
-        // 只保留 UI 和宏功能
-        [[MacroManager shared] setup];
-        [TweakUI showFloatingWindow];
-    }
+    NSLog(@"[GameTweak] dylib loaded!");
 }
