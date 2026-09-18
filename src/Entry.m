13d1b946de91dedfb14a14c5856306c6a6df8b3e
isisks29 <3637065792@qq.com>
2026-09-19 01:25:58 +0800

    Update Entry.m

--- src/Entry.m
+++ src/Entry.m
@@ -1,17 +1,32 @@
 #include <dlfcn.h>
+#import <dlfcn.h>
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "Config.h"
 #import "MacroManager.h"
+#import "UI/TweakUI.h"
 
 __attribute__((constructor))
 void dylib_initialize(void)
 {
     @autoreleasepool {
-        NSLog(@"[GT] dylib constructor enter");
         [[GlobalConfig shared] load];
-        NSLog(@"[GT] GlobalConfig load done");
-        
         [[MacroManager shared] setup];
-        NSLog(@"[GT] MacroManager setup done");
+        
+        __block id noteObserver = nil;
+        noteObserver = [[NSNotificationCenter defaultCenter]
+            addObserverForName:UIApplicationDidFinishLaunchingNotification
+                        object:nil
+                         queue:[NSOperationQueue mainQueue]
+                    usingBlock:^(NSNotification * _Nonnull note) {
+            if(noteObserver){
+                [[NSNotificationCenter defaultCenter] removeObserver:noteObserver];
+                noteObserver = nil;
+            }
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                NSLog(@"[GT] try show floating ui");
+                [TweakUI showFloatingWindow];
+            });
+        }];
     }
 }
